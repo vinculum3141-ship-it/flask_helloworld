303170096b34af421c2e3034a6735adc8749d20b
Ruby van Rooyen <ruby.vanrooyen@gmail.com>
2025-10-16 11:38:29 +0200

    minor updates

--- flask-k8s/app.py
+++ flask-k8s/app.py
@@ -1,5 +1,8 @@
 """
-This code defines a simple web application using Flask. It creates a single route at the root URL ("/") that returns the message "Hello from Flask on Kubernetes (Minikube)!". When run directly, it starts a web server listening on all network interfaces at port 5000. This is typically used as a minimal example for deploying Flask apps, such as in a Kubernetes environment.
+This code defines a simple web application using Flask.
+It creates a single route at the root URL ("/") that returns the message "Hello from Flask on Kubernetes (Minikube)!".
+When run directly, it starts a web server listening on all network interfaces at port 5000.
+This is typically used as a minimal example for deploying Flask apps, such as in a Kubernetes environment.
 """
 from flask import Flask
 app = Flask(__name__)
